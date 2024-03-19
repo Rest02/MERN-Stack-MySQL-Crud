0b919b00df206ed6a47c4dcf8088b6e303540a22
Rest <code.rest.dev@gmail.com>
2024-03-18 22:53:39 -0300

    0.0.3 POST - /tasks

--- server/controllers/tasks.controllers.js
+++ server/controllers/tasks.controllers.js
@@ -1,3 +1,5 @@
+import { pool } from "../db.js";
+
 export const getTasks = (req, res) => {
   res.send("Obteniendo tareas");
 };
@@ -6,8 +8,18 @@ export const getTask = (req, res) => {
   res.send("Obteniendo una tarea");
 };
 
-export const createTask = (req, res) => {
-  res.send("Creando tarea");
+export const createTask = async (req, res) => {
+  const { title, description } = req.body;
+  const [result] = await pool.query(
+    "INSERT INTO tasks(title, description) VALUES(?,?)",
+    [title, description]
+  );
+  console.log(result);
+  res.json({
+    id : result.insertId,
+    title,
+    description,
+  });
 };
 
 export const updateTask = (req, res) => {
