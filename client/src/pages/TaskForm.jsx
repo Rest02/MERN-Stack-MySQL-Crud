203c6a31e82e05ce2cfe23153af6afed30ad8035
Rest <code.rest.dev@gmail.com>
2024-03-22 16:33:30 -0300

    0.0.8 FrontEnd Setup

---
+++ client/src/pages/TaskForm.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function TaskForm() {
+  return (
+    <div>TaskForm component</div>
+  )
+}
+
+export default TaskForm
---
+++ client/src/pages/TaskPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function TaskPage() {
+  return (
+    <div>TaskPage componente</div>
+  )
+}
+
+export default TaskPage
